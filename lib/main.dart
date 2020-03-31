21647510be66ea7f7aae575611b5c3e9c5be353b
aranirahan <aranirahan@gmail.com>
2020-04-01 06:31:33 +0700

    09 - List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,29 +10,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int number = 0;
+  List<Widget> widgets = [];
+  int counter = 1;
+
+  /*_MyAppState() {
+    for (int i = 0; i < 15; i++) {
+      widgets.add(Text(
+        "Data " + i.toString(),
+        style: TextStyle(fontSize: 45),
+      ));
+    }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("07 Anonymous Method"),
-        ),
-        body: Center(
-            child: Text(
-          "Ini adalah text style",
-          style: TextStyle(
-              fontFamily: "Lato-Black",
-              fontSize: 30,
-              fontStyle: FontStyle.italic,
-              decoration: TextDecoration.overline,
-              decorationColor: Colors.red,
-              decorationThickness: 5,
-              decorationStyle: TextDecorationStyle.wavy //gelombang
+          appBar: AppBar(
+            title: Text("08 List View"),
+          ),
+          body:
+              /* Column(
+            */ /* children: <Widget>[
+              Text("Data ke 1"),
+              Text("Data ke 2"),
+              Text("Data ke 3"),
+              Text("Data ke 4"),
+              Text("Data ke 5")
+            ],*/ /*
+            children: widgets,
+          )*/
+              ListView(
+            /*children: widgets,*/
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //sisa space dibagirata
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("Tambah Data"),
+                    onPressed: () {
+                      setState(() {
+                        widgets.add(Text(
+                          "Data " + counter.toString(),
+                          style: TextStyle(fontSize: 35),
+                        ));
+                        counter++;
+                      });
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text("Hapus Data"),
+                    onPressed: () {
+                      setState(() {
+                        widgets.removeLast();
+                        counter--;
+                      });
+                    },
+                  ),
+                ],
               ),
-        )),
-      ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widgets,
+              )
+            ],
+          )),
     );
   }
 }
