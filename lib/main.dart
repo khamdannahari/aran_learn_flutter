fa339983334488322d231914f1bb1b7ab4faacd9
aranirahan <aranirahan@gmail.com>
2020-04-01 08:02:03 +0700

    14 - Spacer widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,30 @@ class MyApp extends StatelessWidget {
           title: Text("Latihan image"),
         ),
         body: Center(
-          child: Container(
-              color: Colors.black,
-              width: 200,
-              height: 200,
-              padding: EdgeInsets.all(10),
-              child: Image(
-                /*image: NetworkImage(
-                  "https://m.ayobandung.com/images-bandung/post/articles/2019/09/09/63113/blackpink_2.jpg"),
-//              fit: BoxFit.none, //menapilkan ukuran gambar aslinya
-              fit: BoxFit.contain,
-              //menyesuaikan ukuran container, rasio masih spt aslinya
-//              fit: BoxFit.cover, //menyesuaikan ukuran container, rasio masih spt aslinya, tidak meninggalkan runag kosong dr container, (center crop)
-//              fit: BoxFit.fill, //menyesuaikan ukuran container, rasio tidak spt aslinya, tidak meninggalkan runag kosong dr container
-              repeat: ImageRepeat.repeat,
-            ),*/
-                image: AssetImage("images/arancrop.png"),
-              )),
-        ),
+            child: Row(
+//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Spacer(flex: 1,),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.red,
+            ),
+            Spacer(flex: 2,),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.green,
+            ),
+            Spacer(flex: 3,),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.blue,
+            ),
+            Spacer(flex: 2,),
+          ],
+        )),
       ),
     );
   }
